668a76239de1ce838f2f3f12ea7f0c10b4afa8c4
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-02-04 10:47:03 +0000

    アドレス変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.6.0'
 set :application, 'sample'
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/tokunaga3/text_aws'
+set :repo_url, 'https://github.com/tokunaga3/text_aws.git'
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
 # deploy先のディレクトリ。
